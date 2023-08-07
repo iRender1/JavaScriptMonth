40febc0c6c33c199b2e6bdc698bd7d862b24e20f
Sultan-Alotaibi <sultan8761s@gmail.com>
2023-08-07 19:58:19 +0300

    Day1

---
+++ day1/script.js
@@ -0,0 +1,31 @@
+// التمرين ألاول
+let age = 15; 
+age = 16;
+
+let name = "Sultan";
+name = "Muslat"
+
+console.log(age , name);
+// التمرين الثاني
+
+// الثوابت مايميدك تغير لا قيمتها ولا اسمها
+
+const al_otaibi511 = 511;
+// al_otaibi511 = 5111;
+console.log(al_otaibi511);
+// التمرين الثالث
+
+// داخل السكوب مايشتغلون الكونست والليت
+
+{
+    var al_otaibi4 = 911;
+}
+// {
+//     const al_otaibi =  1;
+// }
+// {
+//     let al_otaibi2 = 511;
+// }
+console.log(al_otaibi4);
+// console.log(al_otaibi); 
+// console.log(al_otaibi2);
