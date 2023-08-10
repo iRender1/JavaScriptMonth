c760e4e0aafcc89eab7ea1fa41eab25df3e008aa
Sultan-Alotaibi <sultan8761s@gmail.com>
2023-08-10 17:02:50 +0300

    day5

---
+++ day5/script.js
@@ -0,0 +1,299 @@
+// بالبدايه خذينا اللوق
+console.log("Hello , World!!");
+console.log("Name , Sultan Muslat!!");
+console.log("Age , 16!!");
+
+// خذينا نوعين من كتابة المتغيرات
+// explict
+let name = "sultan";
+// imlicit
+let myname;
+myname = "sultan";
+console.log(name , myname);
+
+// غير عن انواع كتابة المتغيرات في ال المتغيرات والثوابت
+// المتغيرات
+let id = "X";
+// الvar غير مستخدم لانه قابل لاعادة التعين وبيبعص عليك الشفل
+var ok = "Q";
+// الثوابت
+const date = "1918/01/01";
+
+// // boolean
+// let test1 = false;
+// let test2 = true;
+// console.log(test1);
+// console.log(test2);
+// console.log(typeof test1);
+
+// // symbol
+// let id = Symbol(4)
+// let id1 = Symbol(4)
+// console.log(id === id1);
+// // console.log(id);
+// // console.log(typeof id);
+
+// // null
+// let text = null;
+// console.log(text);
+// console.log(typeof text);
+
+// // undefined
+// let test5;
+// console.log(test5);
+// console.log(typeof test5);
+
+// // number
+
+// // integer
+// let x = 511;
+// console.log(x);
+// console.log(typeof x);
+
+// // double 
+// let x2 = 5.11;
+// console.log(x2);
+// console.log(typeof x2);
+
+// // bigint type
+// let big = 123456789n;
+// console.log(big);
+// console.log(typeof big);
+
+// let big2 = 1022n - 511n
+// console.log(big2);
+// console.log(typeof big2);
+
+// // string
+// let username = "sultan";
+// console.log(username);
+// console.log(typeof username);
+
+// let numbers = '511';
+// console.log(numbers);
+// console.log(typeof numbers);
+
+// let message = "sultan 3mk 'irender' 511"
+
+// let message2 = 'sultan "irender" 511'
+// console.log(message);
+// console.log(typeof message);
+// console.log(message2);
+// console.log(typeof message2);
+
+// let message3 = "now i become \'death\'"
+
+// let message4 = 'now i become \"death\"'
+// console.log(message3);
+// console.log(typeof message3);
+// console.log(message4);
+// console.log(typeof message4);
+
+// let messsage = "5" - "1"
+
+// let messsage3 = "25" + "5"
+
+// let messsage2 = "death" - "live"
+// console.log(messsage);
+// console.log(typeof messsage);
+// console.log(messsage3);
+// console.log(typeof messsage3);
+// console.log(messsage2);
+// console.log(typeof messsage2);
+
+// // interpolation
+
+// let country = "Riyadh";
+// let continent = "Asia";
+// console.log(`${country} located in ${continent}`);
+
+// let str = "java script language";
+// console.log(str);
+// console.log(str.length);
+// console.log(str.charAt(3));
+// console.log(str.slice(5,11));
+// console.log(str.split(" ",2));
+
+// يوم جديد واكواد جديده اليوم الرابع
+
+// // conversion types
+// let str511 = String();
+// let num = Number();
+// let bool = Boolean();
+
+
+// // conversion string
+
+// // string to string
+// const Str = "text"
+// const strtostr = String(Str)
+// console.log(`${typeof Str} : ${Str}`);
+// console.log(`${typeof strtostr} : ${strtostr}`);
+
+// // num to string
+
+// const num_1 = 20;
+// const num_1_ = String(num_1)
+// console.log(`${typeof num_1} : ${num_1}`);
+// console.log(`${typeof num_1_} : ${num_1_}`);
+
+// // boolen to string
+
+// const boolen = true;
+// const booltostr = String(boolen)
+// console.log(`${typeof boolen} : ${boolen}`);
+// console.log(`${typeof booltostr} : ${booltostr}`);
+
+// // undefined to string
+
+// const undefined_2 = undefined;
+// const undefined_1 = String(undefined_2)
+// console.log(`${typeof undefined_2} : ${undefined_2}`);
+// console.log(`${typeof undefined_1} : ${undefined_1}`);
+
+// // null to string
+
+// const nulll = null;
+// const nulli = String(nulll)
+// console.log(`${typeof nulll} : ${nulll}`);
+// console.log(`${typeof nulli} : ${nulli}`);
+
+// // bigint to string
+
+// const bigint1 = 511n;
+// const bigint2 = String(bigint1)
+// console.log(`${typeof bigint1} : ${bigint1}`);
+// console.log(`${typeof bigint2} : ${bigint2}`);
+
+// // empty to Boolean
+
+// const emptyy = "";
+// const emptyy_ = String(emptyy)
+// console.log(`${typeof emptyy} : ${emptyy}`);
+// console.log(`${typeof emptyy_} : ${emptyy_}`);
+
+// // conversion number
+
+// // string to number
+// const Str_ = "text"
+// const strtonum_ = Number(Str_)
+// console.log(`${typeof Str_} : ${Str_}`);
+// console.log(`${typeof strtonum_} : ${strtonum_}`);
+
+// // num to number
+
+// const num1_ = 20;
+// const num_1__ = Number(num1_)
+// console.log(`${typeof num1_} : ${num1_}`);
+// console.log(`${typeof num_1__} : ${num_1__}`);
+
+// // boolen to number
+
+// const boolen_ = true;
+// const booltonum_ = Number(boolen_)
+// console.log(`${typeof boolen_} : ${boolen_}`);
+// console.log(`${typeof booltonum_} : ${booltonum_}`);
+
+// // undefined to number
+
+// const undefined_2_ = undefined;
+// const undefined_1_ = Number(undefined_2_)
+// console.log(`${typeof undefined_2_} : ${undefined_2_}`);
+// console.log(`${typeof undefined_1_} : ${undefined_1_}`);
+
+// // null to number
+
+// const nullll = null;
+// const nullii = Number(nullll)
+// console.log(`${typeof nullll} : ${nullll}`);
+// console.log(`${typeof nullii} : ${nullii}`);
+
+// // bigint to number
+
+// const bigint1_ = 511n;
+// const bigint2_ = Number(bigint1_)
+// console.log(`${typeof bigint1_} : ${bigint1_}`);
+// console.log(`${typeof bigint2_} : ${bigint2_}`);
+
+// // empty to number
+
+// const empty_ = "";
+// const empty__ = Number(empty_)
+// console.log(`${typeof empty_} : ${empty_}`);
+// console.log(`${typeof empty__} : ${empty__}`);
+
+// // conversion Boolean
+
+// // string to Boolean
+// const Str__ = "text"
+// const strtobool_ = Boolean(Str__)
+// console.log(`${typeof Str__} : ${Str__}`);
+// console.log(`${typeof strtobool_} : ${strtobool_}`);
+
+// // num to Boolean
+
+// const num1__ = 20;
+// const num__1__ = Boolean(num1__)
+// console.log(`${typeof num1__} : ${num1__}`);
+// console.log(`${typeof num__1__} : ${num__1__}`);
+
+// // boolen to Boolean
+
+// const boolen__ = true;
+// const booltonum__ = Boolean(boolen__)
+// console.log(`${typeof boolen__} : ${boolen__}`);
+// console.log(`${typeof booltonum__} : ${booltonum__}`);
+
+// // undefined to Boolean
+
+// const undefined_2__ = undefined;
+// const undefined_1__ = Boolean(undefined_2__)
+// console.log(`${typeof undefined_2__} : ${undefined_2__}`);
+// console.log(`${typeof undefined_1__} : ${undefined_1__}`);
+
+// // null to Boolean
+
+// const nulllll = null;
+// const nulliii = Boolean(nulllll)
+// console.log(`${typeof nulllll} : ${nulllll}`);
+// console.log(`${typeof nulliii} : ${nulliii}`);
+
+// // bigint to Boolean
+
+// const bigint1___ = 511n;
+// const bigint2___ = Boolean(bigint1___)
+// console.log(`${typeof bigint1___} : ${bigint1___}`);
+// console.log(`${typeof bigint2___} : ${bigint2___}`);
+
+// // empty to Boolean
+
+// const emptyyy = "";
+// const emptyyyy = Boolean(emptyyy)
+// console.log(`${typeof emptyyy} : ${emptyyy}`);
+// console.log(`${typeof emptyyyy} : ${emptyyyy}`);
+
+
+// // Automatic Conversion
+
+// const math5 = 20 + "5";
+// console.log(`${typeof math5} : ${math5}`);
+
+// const math55 = 20 - "5";
+// console.log(`${typeof math55} : ${math55}`);
+
+// // طريقة كتابة الاوبجكت
+
+// // non primitive
+
+// // objects
+
+// let objects = {
+//     str: "test",
+//     num: 511,
+//     bool: true
+// };
+// delete objects.str
+// console.log(`${typeof objects} : ${objects.str} ${objects.num} ${objects.bool}`);
+
+// الفرق بين كتابة سكوب عادي وكتابة اوبجكت هو باللوق السكوب العادي ماله اسم متغير بعكس الاوبجكت له اسم متغير وسكوب وبكذا يكون لم حق وصول وبس
+
