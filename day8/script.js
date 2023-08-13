a30ee75d7cd3d7c6af15cb47d630667ccde92630
iRender1 <86774748+iRender1@users.noreply.github.com>
2023-08-13 23:25:46 +0300

    day8

---
+++ day8/script.js
@@ -0,0 +1,11 @@
+let names = ["sultan","muslat","ibrahim","ziyad","ali"]
+console.log(names);
+console.log(names[2]);
+names.unshift("chess")
+console.log(names);
+names.push("grandmaster")
+console.log(names);
+names.shift()
+console.log(names);
+names.pop()
+console.log(names);
