5d67153afa1d5cd8dc14d55a202eaf8649b42756
iRender1 <86774748+iRender1@users.noreply.github.com>
2023-08-20 20:40:35 +0300

    function

---
+++ function/script.js
@@ -0,0 +1,19 @@
+// // التمرين الاول
+// function javascript_Boot_camp(teach) {
+//     alert("This javaScript BootCamp with " + teach + " OK?")
+// }
+
+// javascript_Boot_camp("Teacher1");
+// javascript_Boot_camp("Teacher2");
+
+// // التمرين الثاني
+// function numbers(number1 , number2) {
+//     let number = (number1 + number2) / 2;
+//     alert("المتوسط الحسابي : " + number);
+// }
+
+// // integer
+// numbers(4 , 6);
+
+// // double
+// numbers(4.3 , 6.8);
